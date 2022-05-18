062685f2b1f42adc1c7bb37a07702b03d11554d8
Keshan1122 <krishnapokharel.2022@gmail.com>
2022-05-18 14:48:23 +0545

    Offcanvas Submenu Creation Started

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,84 +21,75 @@
   });
   // Submenu on Hover
 
-  $(".has-children").hover(function(){
-    $("#women-pane").css("display","none");
-    $("#men-pane").css("display","none")
-    $("#accessories-pane").css("display","none")
-    $("#world-pane").css("display","none")
-  })
+  $(".has-children").hover(function () {
+    $("#women-pane").css("display", "none");
+    $("#men-pane").css("display", "none");
+    $("#accessories-pane").css("display", "none");
+    $("#world-pane").css("display", "none");
+  });
 
   // women sub-menu
-  $("#sub-menu-women").hover(function(){
-    $("#women-pane").css("display","block");
-    $("#men-pane").css("display","none")
-    $("#accessories-pane").css("display","none")
-    $("#world-pane").css("display","none")
-  }
-  
-  );
-  $("#women-pane").hover(function(){},
-  function(){
-    $(this).css("display","none")
+  $("#sub-menu-women").hover(function () {
+    $("#women-pane").css("display", "block");
+    $("#men-pane").css("display", "none");
+    $("#accessories-pane").css("display", "none");
+    $("#world-pane").css("display", "none");
   });
-
-
+  $("#women-pane").hover(
+    function () {},
+    function () {
+      $(this).css("display", "none");
+    }
+  );
 
   //  Men Sub-menu
-  $("#sub-menu-men").hover(function(){
-    $("#men-pane").css("display","block");
-    $("#women-pane").css("display","none")
-    $("#accessories-pane").css("display","none")
-    $("#world-pane").css("display","none")
-    
-  }
- 
-  
-  );
-  $("#men-pane").hover(function(){},
-  function(){
-    $(this).css("display","none")
+  $("#sub-menu-men").hover(function () {
+    $("#men-pane").css("display", "block");
+    $("#women-pane").css("display", "none");
+    $("#accessories-pane").css("display", "none");
+    $("#world-pane").css("display", "none");
   });
-
-
+  $("#men-pane").hover(
+    function () {},
+    function () {
+      $(this).css("display", "none");
+    }
+  );
 
   // Accesssories Sub-menu
-  $("#sub-menu-accessories").hover(function(){
-    $("#accessories-pane").css("display","block");
-    
-    $("#men-pane").css("display","none");
-    $("#women-pane").css("display","none");
-   
-    $("#world-pane").css("display","none");
-  }
-  
-  );
-  $("#accessories-pane").hover(function(){},
-  function(){
-    $(this).css("display","none");
+  $("#sub-menu-accessories").hover(function () {
+    $("#accessories-pane").css("display", "block");
+
+    $("#men-pane").css("display", "none");
+    $("#women-pane").css("display", "none");
+
+    $("#world-pane").css("display", "none");
   });
+  $("#accessories-pane").hover(
+    function () {},
+    function () {
+      $(this).css("display", "none");
+    }
+  );
   // Our World Sub menu
-   // Accesssories Sub-menu
-   $("#sub-menu-world").hover(function(){
-    $("#world-pane").css("display","block");
-    
-    $("#men-pane").css("display","none");
-    $("#women-pane").css("display","none");
-   
-    $("#accessories-pane").css("display","none");
-  }
-  
-  );
-  $("#world-pane").hover(function(){},
-  function(){
-    $(this).css("display","none");
+
+  $("#sub-menu-world").hover(function () {
+    $("#world-pane").css("display", "block");
+
+    $("#men-pane").css("display", "none");
+    $("#women-pane").css("display", "none");
+
+    $("#accessories-pane").css("display", "none");
   });
-
-
-  
+  $("#world-pane").hover(
+    function () {},
+    function () {
+      $(this).css("display", "none");
+    }
+  );
 
   // offcanvas search switch
-  
+
   $(".offcanvas-search").on("click", function () {
     $(".search-model").css("display", "block");
   });
@@ -106,7 +97,84 @@
   $(".offcanvas-search-close-switch").on("click", function () {
     $(".search-model").css("display", "none");
   });
+  // offcanvas Sub menu
+  $("#offcanvas-women-li").on("click", function () {
+    $("#women-submenu").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+  });
+  $("#offcanvas-men-li").on("click", function () {
+    $("#men-submenu").toggleClass("active");
+    $("#rotate-icon6").toggleClass("arrow-rotate");
+  });
+  // women submenu ready to wear Section
+  $("#submenu-menu").on("click", function () {
+    $("#women-clothes").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+    $("#women-submenu").toggleClass("active");
 
+    $("#rotate-icon1").toggleClass("arrow-rotate");
+  });
+  // Women Submenu Accessories Section
+  
+  $("#submenu-menu1").on("click", function () {
+    $("#women-accessories").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+    $("#women-submenu").toggleClass("active");
+
+    $("#rotate-icon2").toggleClass("arrow-rotate");
+  });
+  // women submenu Shoe Section
+  $("#submenu-menu2").on("click", function () {
+    $("#women-shoes").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+    $("#women-submenu").toggleClass("active");
+
+    $("#rotate-icon3").toggleClass("arrow-rotate");
+  });
+  // women submenu Bags Section
+  $("#submenu-menu3").on("click", function () {
+    $("#women-bags").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+    $("#women-submenu").toggleClass("active");
+
+    $("#rotate-icon4").toggleClass("arrow-rotate");
+  });
+  // women submenu Home Section
+  $("#submenu-menu4").on("click", function () {
+    $("#women-home").toggleClass("active");
+    $("#rotate-icon").toggleClass("arrow-rotate");
+    $("#women-submenu").toggleClass("active");
+
+    $("#rotate-icon5").toggleClass("arrow-rotate");
+  });
+  // men submenu Ready to wear section
+  $("#submenu-menu5").on("click", function () {
+    $("#men-clothes").toggleClass("active");
+    $("#men-submenu").toggleClass("active");
+    $("#rotate-icon7").toggleClass("arrow-rotate");
+    $("#rotate-icon6").toggleClass("arrow-rotate");
+  });
+  // Men submenu Accessories Section
+  $("#submenu-menu6").on("click", function () {
+    $("#men-accessories").toggleClass("active");
+    $("#men-submenu").toggleClass("active");
+    $("#rotate-icon8").toggleClass("arrow-rotate");
+    $("#rotate-icon6").toggleClass("arrow-rotate");
+  });
+  // Men submenu Shoe Section
+  $("#submenu-menu7").on("click", function () {
+    $("#men-shoes").toggleClass("active");
+    $("#men-submenu").toggleClass("active");
+    $("#rotate-icon9").toggleClass("arrow-rotate");
+    $("#rotate-icon6").toggleClass("arrow-rotate");
+  });
+   // Accessories submenu  Section
+   $("#submenu-menu8").on("click", function () {
+    $("#men-shoes").toggleClass("active");
+    $("#men-submenu").toggleClass("active");
+    $("#rotate-icon9").toggleClass("arrow-rotate");
+    $("#rotate-icon6").toggleClass("arrow-rotate");
+  });
 
   /*------------------
           Background Set
